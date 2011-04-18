a11fab85e24b89a7c762093bdacda6bff7004c3e
Marc Palmer <marc@anyware.co.uk>
2011-04-18 15:41:22 +0100

    Improvements to code comments and example styling

--- example/example.js
+++ example/example.js
@@ -108,5 +108,5 @@ $(document).ready(function() {
     });
     $('.dateValue').datepicker({dateFormat: DATE_FORMAT});
 
-    $('#populateButton').click( function( event ) {horn.populate();});
+    $('a.populateButton').click( function( event ) { horn.populate.call(horn); event.stopPropagation(); });
 });
