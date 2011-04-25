04e831e7d22e65e0cf8b7b0b5ae76a2cc901c5f8
wangjammer7 <wj7@wangjammers.org>
2011-04-25 19:07:45 +0100

    - Removed global scoped rubbish by putting it into the onpageready jq handler function
    Signed-off-by: wangjammer7 <wj7@wangjammers.org>

--- example/example.js
+++ example/example.js
@@ -1,92 +1,94 @@
-var bindings = {};
-var maxID = -1;
 
-var DATE_FORMAT = "dd MM yy";
-
-var makeID = function () {
-    return "hi_" + (++maxID);
-};
-
-var isArray = function( object ) {
-    if ( object === undefined ) { return false; }
-    if ( object === null ) { return false; }
-    if ( typeof object !== 'object' ) { return false; }
-    if ( object.constructor.toString().indexOf( 'Array') < 0) { return false; }
-    return true;
-};
-
-var isObject = function( object ) {
-    if ( object === undefined ) { return false; }
-    if ( object === null ) { return false; }
-    if ( typeof object !== 'object' ) { return false; }
-    if ( object.constructor.toString().indexOf( 'Object') < 0) { return false; }
-    return true;
-};
-
-var converterNameForNode = function( node ) {
-    node = $(node);
-    if ( node.hasClass( 'numberValue') ) {
-        return "IntegerConverter";
-    }
-
-    if ( node.hasClass( 'dateValue') ) {
-        return "DateConverter";
-    }
-
-    if ( node.hasClass( 'booleanValue') ) {
-        return "BooleanConverter";
-    }
-
-    return null;
-};
-
-var indent = function ( count ) {
-    var rv = '';
-    while ( count-- > 0 ) { rv += '&nbsp;&nbsp;&nbsp;&nbsp;'; }
-    return rv;
-};
-
-var span = function ( text, classAttribute ) {
-    return '<span' + ((classAttribute !== undefined) ? (' class=\'' +
-        classAttribute.toString() + '\'') : '') + '>' + text + '</span>';
-};
-
-var input = function ( value, type, id ) {
-    var insert = (type ? "Value" : "");
-    return "<input value='" + value + "' class='dynamic " + type + insert +
-        "'  id='" + id + "' type='text'/>";
-};
-
-
-var render = function ( object, ind, parent, pk ) {
-    var rv;
-    var isO = isObject( object);
-    var isA = isArray( object);
-    if ( ind === undefined ) { ind = 0; }
-    if ( isO || isA ) {
-        rv = (isA ? "[" : "{") + "<br/>"
-        horn.each( object, function( key, value ) {
-            rv +=   indent( ind + 1) + "\"" + key + "\": " +
-                    render( value, ind + 1, object, key) + ",<br/>";
-        });
-        rv += indent( ind) + (isA ? "]" : "}");
-        return  rv;
-    } else {
-        rv = typeof object;
-        isO = makeID();
-        isA = "";
-
-        if ( object instanceof Date ) { rv = 'date'; }
-        if ( rv === 'string' ) { isA = "\"" + input( object, null, isO) + "\""; } else
-        if ( rv === 'boolean' ) { isA = input( object ? "Yes" : "No", rv, isO); } else
-        if ( rv === 'number' ) { isA = input( object, rv, isO); } else
-        if ( rv === 'date' ) { isA = input( $.datepicker.formatDate( DATE_FORMAT, object), rv, isO); }
-        if ( isA !== "" ) { bindings[ isO] = {parent: parent, pk: pk, type: rv + "Value"}; }
-        return isA;
-    }
-};
 
 $(document).ready(function() {
+    var bindings = {};
+    var maxID = -1;
+
+    var DATE_FORMAT = "dd MM yy";
+
+    var makeID = function () {
+        return "hi_" + (++maxID);
+    };
+
+    var isArray = function( object ) {
+        if ( object === undefined ) { return false; }
+        if ( object === null ) { return false; }
+        if ( typeof object !== 'object' ) { return false; }
+        if ( object.constructor.toString().indexOf( 'Array') < 0) { return false; }
+        return true;
+    };
+
+    var isObject = function( object ) {
+        if ( object === undefined ) { return false; }
+        if ( object === null ) { return false; }
+        if ( typeof object !== 'object' ) { return false; }
+        if ( object.constructor.toString().indexOf( 'Object') < 0) { return false; }
+        return true;
+    };
+
+    var converterNameForNode = function( node ) {
+        node = $(node);
+        if ( node.hasClass( 'numberValue') ) {
+            return "IntegerConverter";
+        }
+
+        if ( node.hasClass( 'dateValue') ) {
+            return "DateConverter";
+        }
+
+        if ( node.hasClass( 'booleanValue') ) {
+            return "BooleanConverter";
+        }
+
+        return null;
+    };
+
+    var indent = function ( count ) {
+        var rv = '';
+        while ( count-- > 0 ) { rv += '&nbsp;&nbsp;&nbsp;&nbsp;'; }
+        return rv;
+    };
+
+    var span = function ( text, classAttribute ) {
+        return '<span' + ((classAttribute !== undefined) ? (' class=\'' +
+            classAttribute.toString() + '\'') : '') + '>' + text + '</span>';
+    };
+
+    var input = function ( value, type, id ) {
+        var insert = (type ? "Value" : "");
+        return "<input value='" + value + "' class='dynamic " + type + insert +
+            "'  id='" + id + "' type='text'/>";
+    };
+
+
+    var render = function ( object, ind, parent, pk ) {
+        var rv;
+        var isO = isObject( object);
+        var isA = isArray( object);
+        if ( ind === undefined ) { ind = 0; }
+        if ( isO || isA ) {
+            rv = (isA ? "[" : "{") + "<br/>"
+            horn.each( object, function( key, value ) {
+                rv +=   indent( ind + 1) + "\"" + key + "\": " +
+                        render( value, ind + 1, object, key) + ",<br/>";
+            });
+            rv += indent( ind) + (isA ? "]" : "}");
+            return  rv;
+        } else {
+            rv = typeof object;
+            isO = makeID();
+            isA = "";
+
+            if ( object instanceof Date ) { rv = 'date'; }
+            if ( rv === 'string' ) { isA = "\"" + input( object, null, isO) + "\""; } else
+            if ( rv === 'boolean' ) { isA = input( object ? "Yes" : "No", rv, isO); } else
+            if ( rv === 'number' ) { isA = input( object, rv, isO); } else
+            if ( rv === 'date' ) { isA = input( $.datepicker.formatDate( DATE_FORMAT, object), rv, isO); }
+            if ( isA !== "" ) { bindings[ isO] = {parent: parent, pk: pk, type: rv + "Value"}; }
+            return isA;
+        }
+    };
+
     horn = new Horn();
 
     horn.option( 'converter', 'IntegerConverter',
